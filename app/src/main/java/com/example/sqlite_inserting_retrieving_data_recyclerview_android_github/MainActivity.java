e4dd5400386fd3999e23995ed550c4d8b4750030
Aman Sharma <amansharma.11@hotmail.com>
2019-06-02 15:29:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sqlite_inserting_retrieving_data_recyclerview_android_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sqlite_inserting_retrieving_data_recyclerview_android_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
