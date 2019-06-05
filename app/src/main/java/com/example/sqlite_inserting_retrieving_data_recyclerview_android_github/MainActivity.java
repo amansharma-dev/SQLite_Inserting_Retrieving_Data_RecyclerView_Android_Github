b5a75f4906356d23d654c3febb2adfecd58a63bf
Aman Sharma <amansharma.11@hotmail.com>
2019-06-05 16:06:12 +0530

    commit

--- app/src/main/java/com/example/sqlite_inserting_retrieving_data_recyclerview_android_github/MainActivity.java
+++ app/src/main/java/com/example/sqlite_inserting_retrieving_data_recyclerview_android_github/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.sqlite_inserting_retrieving_data_recyclerview_android_github;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
+import com.example.sqlite_inserting_retrieving_data_recyclerview_android_github.database.DBHelper;
 import com.example.sqlite_inserting_retrieving_data_recyclerview_android_github.model.DataItem;
 import com.example.sqlite_inserting_retrieving_data_recyclerview_android_github.sample.SampleDataProvider;
 
@@ -14,11 +18,16 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     List<DataItem> dataItemList = SampleDataProvider.dataItemList;
 
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteOpenHelper sqLiteOpenHelper = new DBHelper(this);
+        database = sqLiteOpenHelper.getWritableDatabase();
+        Toast.makeText(getApplicationContext(),"Database Acquired",Toast.LENGTH_SHORT).show();
 
         DataItemAdapter dataItemAdapter = new DataItemAdapter(this,dataItemList);
         recyclerView = findViewById(R.id.recycler_view);
---
+++ app/src/main/java/com/example/sqlite_inserting_retrieving_data_recyclerview_android_github/database/ItemsTable.java
@@ -0,0 +1,25 @@
+package com.example.sqlite_inserting_retrieving_data_recyclerview_android_github.database;
+
+public class ItemsTable {
+
+    public static final String TABLE_NAME = "items";
+    public static final String COLUMN_ID = "itemId";
+    public static final String COLUMN_ARTIST_IMAGE = "artistImage";
+    public static final String COLUMN_SONG_NAME = "songName";
+    public static final String COLUMN_ARTIST_NAME = "artistName";
+    public static final String COLUMN_ARTIST_GENRE = "artistGenre";
+    public static final String COLUMN_SONG_PRICE = "songPrice";
+
+    public static final String SQL_CREATE =
+            "CREATE TABLE " + TABLE_NAME + "(" +
+                    COLUMN_ID + " TEXT PRIMARY KEY," +
+                    COLUMN_ARTIST_IMAGE + " TEXT," +
+                    COLUMN_SONG_NAME + " TEXT," +
+                    COLUMN_ARTIST_NAME + " TEXT," +
+                    COLUMN_ARTIST_GENRE + " TEXT," +
+                    COLUMN_SONG_PRICE + " REAL" + ");";
+
+    public static final String SQL_DELETE =
+            "DROP TABLE " + TABLE_NAME;
+
+}
